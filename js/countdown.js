093b1d10558c3fc35c4b62e4523296e7f58bf381
vandywiranata <35490039+vandywiranata@users.noreply.github.com>
2019-09-22 23:36:07 +0700

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -44,22 +44,3 @@ function initializeClock(id, endtime) {
 
 var deadline = new Date;
 initializeClock('clockdiv');
-
-///////////////
-
-// var x = setInterval(function() {
-//   var now = new Date().getTime();
-//   var distance = countDownDate - now;
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   document.getElementById("clockdiv").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("clockdiv").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
